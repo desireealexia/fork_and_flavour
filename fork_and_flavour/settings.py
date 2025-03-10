6bc750bc81c083387e90012969ac9f2b331eca16
desireealexia <desireealexia19@gmail.com>
2025-03-10 16:22:21 +0000

    Created superuser for admin access

--- fork_and_flavour/settings.py
+++ fork_and_flavour/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d4y4cx)$%7-i6t*fqj3vc(my$!c1c%j)glu8^z@12291%12q$4'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
